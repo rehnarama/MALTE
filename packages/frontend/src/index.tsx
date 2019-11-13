cf21004fe33cfa3bb4fa44cebd55b6d76bd6fd26
Michael Rehn <rehn.michael@outlook.com>
2019-11-13 17:18:40 +0100

    Fix minimal xterm example

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Terminal } from "xterm";
+import "xterm/css/xterm.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const terminal = new Terminal();
+var terminalNode = document.getElementById("terminal");
+if (terminalNode !== null) {
+  terminal.open(terminalNode);
+  terminal.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
