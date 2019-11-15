e7f80d4967fb02ac85d1135090ce100f8b332639
Michael Rehn <rehn.michael@outlook.com>
2019-11-15 11:50:28 +0100

    Add basic WebSocket connection

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,16 +1,22 @@
-
-import express from 'express';
+import express from "express";
+import socketio from "socket.io";
 
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('YAY! It actually works!');
+app.get("/", (req, res) => {
+  res.send("YAY! It actually works!");
 });
 
-app.listen(port, err => {
+const server = app.listen(port, err => {
   if (err) {
     return console.error(err);
   }
   return console.log(`Server is listening on ${port}`);
+});
+
+const io = socketio(server);
+io.on("connection", socket => {
+  console.log(`Socket with id ${socket.id} connected`);
+  socket.emit("hello-world", { hello: "world" });
 });
