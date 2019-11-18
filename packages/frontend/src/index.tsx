53537d9e6f4a87d26209522d3afc867cd7840f4e
Michael <rehn.michael@outlook.com>
2019-11-18 11:02:40 +0100

    Add basic WebSocket connection (#48)
    * Add basic WebSocket connection
    
    * Add default backend url

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,5 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import io from "socket.io-client";
+
+let backendUrl = "localhost:4000";
+if (process.env.REACT_APP_BACKEND_URL) {
+  backendUrl = process.env.REACT_APP_BACKEND_URL;
+}
+
+const socket = io(backendUrl);
+socket.on("connect", () => {
+  console.log("Connected to server");
+});
+socket.on("hello-world", (data: any) => {
+  console.log("hello world was sent", data);
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
