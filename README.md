33ae651cc8a75c79801d900d4e3f9e53462b265a
Michael <rehn.michael@outlook.com>
2019-11-18 22:23:59 +0100

    Add style and lint checks (#53)
    * Set up prettier check
    
    * Fix quotation mark
    
    * Fix all styling errors
    
    * Add eslint to frontend
    
    * Fix eslint configuration, add lint script
    
    * Add eslint config in backend
    
    * Configure prettier eslint integration for frontend/backend
    
    * Add github action for linting
    
    * Add documentation
    
    * Fix typo in linting action
    
    * Change install to ci in lint action

--- README.md
+++ README.md
@@ -1,10 +1,53 @@
 Website: https://rehnarama.github.io/MALTE/index.html
 
 # MALTE
+
 Multi Access Live Text Editor
 
 Project in Software Engineering 2019 - Dream Team
 
+# Development
+
+The code is structured into several separate projects, all of which resides
+in the folder `packages`.
+
+We manage all projects with node, which means you can install all dependencies
+by simply typing `npm install` in each project folder.
+
+## Environment Variables (Frontend)
+* `REACT_APP_BACKEND_URL`: (default: `localhost:3000`). The backend URL. All requests and WebSocket connections will be established with this URL
 
 # How to Run
+
 Execute `docker-compose up`. Frontend will run at port `3000` and backend at port `4000`
+
+# Code Styling
+
+We use `prettier` for styling and `eslint` for code checks. `eslint` is 
+configured with their recommended configuration as well as checking for the
+default prettier configuration. You can run all checks by running the command
+```sh
+npm run lint
+```
+in the project folder for `frontend` and `backend`.
+
+The eslint plugin for Visual Studio Code does check eslint errors in typescript
+files by default. You have to enable this with the following configuration in 
+Visual Studio Code:
+```json
+{
+  "eslint.validate": [
+    "javascript",
+    "javascriptreact",
+    {
+      "autoFix": true,
+      "language": "typescript"
+    },
+    {
+      "autoFix": true,
+      "language": "typescriptreact"
+    }
+  ]
+}
+```
+
--- packages/rga/src/RGAIdentifier.test.ts
+++ packages/rga/src/RGAIdentifier.test.ts
@@ -7,45 +7,33 @@ describe("RGAIdentifier", function() {
     const a = new RGAIdentifier(0, 0);
     const b = new RGAIdentifier(0, 1);
 
-    assert.isTrue(
-        a.compareTo(b) < 0
-    );
+    assert.isTrue(a.compareTo(b) < 0);
   });
   it("compareTo should return negative if a precedes b with different sid, same sum", () => {
     const a = new RGAIdentifier(0, 0);
     const b = new RGAIdentifier(1, 0);
-    assert.isTrue(
-        a.compareTo(b) < 0
-    );
+    assert.isTrue(a.compareTo(b) < 0);
   });
   it("compareTo should return negative if a precedes b with different sid, different sum", () => {
     const a = new RGAIdentifier(0, 0);
     const b = new RGAIdentifier(1, 1);
-    assert.isTrue(
-        a.compareTo(b) < 0
-    );
+    assert.isTrue(a.compareTo(b) < 0);
   });
 
   it("compareTo should return positive if b precedes a with same sid, different sum", () => {
     const a = new RGAIdentifier(0, 0);
     const b = new RGAIdentifier(0, 1);
 
-    assert.isTrue(
-        b.compareTo(a) > 0
-    );
+    assert.isTrue(b.compareTo(a) > 0);
   });
   it("compareTo should return positive if b precedes a with different sid, same sum", () => {
     const a = new RGAIdentifier(0, 0);
     const b = new RGAIdentifier(1, 0);
-    assert.isTrue(
-        b.compareTo(a) > 0
-    );
+    assert.isTrue(b.compareTo(a) > 0);
   });
   it("compareTo should return positive if b precedes a with different sid, different sum", () => {
     const a = new RGAIdentifier(0, 0);
     const b = new RGAIdentifier(1, 1);
-    assert.isTrue(
-        b.compareTo(a) > 0
-    );
+    assert.isTrue(b.compareTo(a) > 0);
   });
 });
