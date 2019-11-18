df2f8eb28c42a7dce94b0b8ccaacc5b7c324ea47
Michael Rehn <rehn.michael@outlook.com>
2019-11-18 17:17:18 +0100

    Add fsTree function with tests

---
+++ packages/backend/src/functions/fsTree.ts
@@ -0,0 +1,47 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+export interface TreeNode {
+  name: string;
+  path: string;
+  children?: TreeNode[];
+  type: "directory" | "file";
+}
+
+/**
+ * fsTree Generates a JSON tree of the file system
+ * @argument root The root of the tree
+ * @argument depth How many levels of recursion of folders to go through.
+ *           Infinity for an infinite amount of recursion
+ */
+export default async function fsTree(
+  root: string,
+  depth: number = Infinity
+): Promise<TreeNode> {
+  const stats = await fs.lstat(root);
+
+  if (stats.isFile()) {
+    return {
+      name: path.basename(root),
+      path: path.resolve(root),
+      type: "file"
+    };
+  } else if (stats.isDirectory()) {
+    const suburls = await fs.readdir(root);
+    const obj: TreeNode = {
+      name: path.basename(root),
+      path: path.resolve(root),
+      type: "directory"
+    };
+
+    if (depth > 0) {
+      obj.children = await Promise.all(
+        suburls.map(
+          async suburl => await fsTree(path.join(root, suburl), depth - 1)
+        )
+      );
+    }
+
+    return obj;
+  }
+}
