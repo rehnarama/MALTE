ebea0ba31ee62bb4d31dc609f1cc08318b5c7cd6
AdamInersjo <adam.inersjo@hotmail.com>
2019-11-18 07:50:11 +0100

    Move Editor component into CodeEditor

--- packages/frontend/src/CodeEditor.tsx
+++ packages/frontend/src/CodeEditor.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
+import {default as Monaco} from '@monaco-editor/react';
+
 
 const CodeEditor: React.FC = () => {
-  return (
-    <div>
-      <p>CodeEditor</p>
-    </div>
-  );
+  return <Monaco height="75vh" width="80vw" language="javascript" />;
 }
 
 export default CodeEditor;
