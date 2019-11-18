a67c9c1754b4d5d3c430dedbec4f99915367527c
Michael Rehn <rehn.michael@outlook.com>
2019-11-18 08:42:43 +0100

    Add default backend url

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -4,15 +4,17 @@ import "./index.css";
 import App from "./App";
 import io from "socket.io-client";
 
-
+let backendUrl = "localhost:4000";
 if (process.env.REACT_APP_BACKEND_URL) {
-  const socket = io(process.env.REACT_APP_BACKEND_URL);
-  socket.on("connect", () => {
-    console.log("Connected to server");
-  });
-  socket.on("hello-world", (data: any) => {
-    console.log("hello world was sent", data);
-  } );
+  backendUrl = process.env.REACT_APP_BACKEND_URL;
 }
+
+const socket = io(backendUrl);
+socket.on("connect", () => {
+  console.log("Connected to server");
+});
+socket.on("hello-world", (data: any) => {
+  console.log("hello world was sent", data);
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
