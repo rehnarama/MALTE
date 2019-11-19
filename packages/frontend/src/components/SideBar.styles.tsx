afd891fed30224ee0dea0deac7778181fa743c01
jowe3751 <jowe3751@student.uu.se>
2019-11-19 14:52:22 +0100

    Feature/frontend/filetree/38 (#63)
    * Add file tree module
    
    * Update CSS style
    
    * package-lock thing
    
    * packages
    
    * lint fixes
    
    * lint fixes

---
+++ packages/frontend/src/components/SideBar.styles.tsx
@@ -0,0 +1,41 @@
+import { TreeTheme } from "react-treebeard";
+
+export default {
+  tree: {
+    base: {
+      backgroundColor: "transparent"
+    },
+    node: {
+      header: {
+        base: {
+          color: "black",
+          display: "inline-block",
+          verticalAlign: "top"
+        }
+      },
+      toggle: {
+        base: {
+          position: "relative",
+          display: "inline-block",
+          verticalAlign: "top",
+          marginLeft: "-5px",
+          height: "24px",
+          width: "24px"
+        },
+        wrapper: {
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          margin: "-7px 0 0 -7px",
+          height: "14px"
+        },
+        height: 12,
+        width: 12,
+        arrow: {
+          fill: "#000000",
+          strokeWidth: 0
+        }
+      }
+    }
+  }
+} as TreeTheme;
