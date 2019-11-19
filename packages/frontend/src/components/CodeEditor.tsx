1caeb1050a19ebc10ce5d24854ec3d19f921c9c6
AdamInersjo <adam.inersjo@hotmail.com>
2019-11-19 10:10:06 +0100

    Reformat mapping code using ESLint

--- packages/frontend/src/components/CodeEditor.tsx
+++ packages/frontend/src/components/CodeEditor.tsx
@@ -1,11 +1,11 @@
 import React, { useRef } from "react";
-import {default as Monaco, monaco} from '@monaco-editor/react';
-import mapOperations, { printInternalOperations } from "../functions/MapOperations";
-
+import { default as Monaco, monaco } from "@monaco-editor/react";
+import mapOperations, {
+  printInternalOperations
+} from "../functions/MapOperations";
 
 const CodeEditor: React.FC = () => {
   const editorRef = useRef();
-
 
   const handleEditorDidMount = (valueGetter: any, editor: any) => {
     console.log("Editor has loaded!");
@@ -19,15 +19,17 @@ const CodeEditor: React.FC = () => {
     editor.getModel().onDidChangeContent((event: any) => {
       const op = event.changes[0];
       printInternalOperations(mapOperations(op));
-    })
-  }
+    });
+  };
 
-  return <Monaco 
-    height="75vh"
-    width="80vw"
-    language="javascript"
-    editorDidMount={handleEditorDidMount}
-  />;
-}
+  return (
+    <Monaco
+      height="75vh"
+      width="80vw"
+      language="javascript"
+      editorDidMount={handleEditorDidMount}
+    />
+  );
+};
 
 export default CodeEditor;
--- packages/frontend/src/functions/MapOperations.ts
+++ packages/frontend/src/functions/MapOperations.ts
@@ -1,94 +1,97 @@
-
 export interface Range {
-    endColumn: number;
-    endLineNumber: number;
-    startColumn: number;
-    startLineNumber: number;
+  endColumn: number;
+  endLineNumber: number;
+  startColumn: number;
+  startLineNumber: number;
 }
 
 export interface MonacoOperation {
-    range: Range;
-    rangeLength: number;
-    rangeOffset: number;
-    text: string;
+  range: Range;
+  rangeLength: number;
+  rangeOffset: number;
+  text: string;
 }
 
 export enum Operation {
-    Insert, 
-    Remove
+  Insert,
+  Remove
 }
 
 export interface RemoveOperation {
-    type: Operation.Remove;
-    position: number;
+  type: Operation.Remove;
+  position: number;
 }
 
 export interface InsertOperation {
-    type: Operation.Insert;
-    position: number;
-    character: string;
+  type: Operation.Insert;
+  position: number;
+  character: string;
 }
 
 export type InternalOperation = RemoveOperation | InsertOperation;
 
 /**
  * Converts an operation from the Monaco Editor to Internal operations
- * 
+ *
  * Removals are done from right-to-left and insertions are done left-to-right.
  * Replacing characters, e.g. autocompletions, are converted to Remove operations
  * followed by Insert operations.
- * 
+ *
  * @param op the change operation aquired from 'onDidChangeContent' listener
  * @return   a list of internal operations that correspond to op
  */
 function mapOperations(op: MonacoOperation): InternalOperation[] {
-    if (isRemoveOperation(op)) {
-        return mapRemoveOperation(op);
-    } else if (isReplaceOperation(op)) {
-        return mapReplaceOperation(op);
-    } else {
-        return mapInsertOperation(op);
-    }
+  if (isRemoveOperation(op)) {
+    return mapRemoveOperation(op);
+  } else if (isReplaceOperation(op)) {
+    return mapReplaceOperation(op);
+  } else {
+    return mapInsertOperation(op);
+  }
 }
 
 export default mapOperations;
 
 function isRemoveOperation(op: MonacoOperation): boolean {
-    return op.rangeLength > 0 && op.text === "";
+  return op.rangeLength > 0 && op.text === "";
 }
 
 function isReplaceOperation(op: MonacoOperation): boolean {
-    return op.rangeLength > 0 && op.text !== ""; 
+  return op.rangeLength > 0 && op.text !== "";
 }
 
 function mapRemoveOperation(op: MonacoOperation): InternalOperation[] {
-    let newOps: InternalOperation[] = [];
-    for (let i = op.rangeLength - 1; i >= 0; i--) {
-        newOps.push({type: Operation.Remove, position: op.rangeOffset + i});
-    }
-    return newOps;
+  let newOps: InternalOperation[] = [];
+  for (let i = op.rangeLength - 1; i >= 0; i--) {
+    newOps.push({ type: Operation.Remove, position: op.rangeOffset + i });
+  }
+  return newOps;
 }
 
 function mapInsertOperation(op: MonacoOperation): InternalOperation[] {
-    let newOps: InternalOperation[] = [];
-    for (let i = 0; i < op.text.length; i++) {
-        newOps.push({ type: Operation.Insert, position: op.rangeOffset + i, character: op.text.charAt(i) });
-    }
-    return newOps;
+  let newOps: InternalOperation[] = [];
+  for (let i = 0; i < op.text.length; i++) {
+    newOps.push({
+      type: Operation.Insert,
+      position: op.rangeOffset + i,
+      character: op.text.charAt(i)
+    });
+  }
+  return newOps;
 }
 
 function mapReplaceOperation(op: MonacoOperation): InternalOperation[] {
-    const removeOps = mapRemoveOperation(op);
-    const insertOps = mapInsertOperation(op);
-    return removeOps.concat(insertOps);
+  const removeOps = mapRemoveOperation(op);
+  const insertOps = mapInsertOperation(op);
+  return removeOps.concat(insertOps);
 }
 
 export function printInternalOperations(ops: InternalOperation[]) {
-    for (const op of ops) {
-        if (op.type === Operation.Insert) {
-            console.log("Insert '" + op.character + "' at position " + op.position);
-        } else {
-            console.log("Remove at position " + op.position);
-        }
+  for (const op of ops) {
+    if (op.type === Operation.Insert) {
+      console.log("Insert '" + op.character + "' at position " + op.position);
+    } else {
+      console.log("Remove at position " + op.position);
     }
+  }
 }
