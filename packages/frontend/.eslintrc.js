f4d873624e0de2b0c18719d27479f8e39d305e2a
Michael <rehn.michael@outlook.com>
2019-11-19 11:15:14 +0100

    Fix eslint rule to use typescript variants (#60)

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended"
   ],
   globals: {
