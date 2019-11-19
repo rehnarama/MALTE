6477686d132dba23ac13006b385cf6ec001d4c2f
Jonas Norlinder <jonas@norlinder.nu>
2019-11-19 14:44:43 +0100

    Implement Terminal Support in Backend and Frontend (#55)
    * Add Terminal skeleton
    
    * Working terminal in backend and frontend
    
    * Add documentation
    
    * Add kill and test termination
    
    * Style
    
    * Fix style
    
    * Add CORS and updated README.md. Style fix
    
    * Fix style
    
    * Update style
    
    * Update packages/backend/src/index.ts
    
    Co-Authored-By: Michael <rehn.michael@outlook.com>
    
    * Update packages/backend/src/index.ts
    
    Co-Authored-By: Michael <rehn.michael@outlook.com>
    
    * Fix URL

---
+++ packages/backend/src/functions/terminal/Terminal.ts
@@ -0,0 +1,37 @@
+import * as pty from "node-pty";
+import socketio from "socket.io";
+
+/**
+ * Create a pseudo-terminal and pipe the std-out/in to
+ * a socket.
+ *
+ * @argument socket The socket to pipe data on.
+ */
+class Terminal {
+  private terminal: pty.IPty;
+
+  constructor(socket: socketio.Socket) {
+    this.terminal = pty.spawn("bash", [], {
+      name: "xterm-color",
+      env: process.env,
+      cwd: process.env.HOME
+    });
+
+    this.terminal.on("data", function(data) {
+      socket.emit("pty-data", data.toString());
+    });
+
+    socket.on("pty-data", data => {
+      this.terminal.write(data);
+    });
+  }
+
+  /**
+   * Kill the terminal
+   */
+  public kill(): void {
+    this.terminal.kill();
+  }
+}
+
+export default Terminal;
--- packages/frontend/src/components/Terminal.tsx
+++ packages/frontend/src/components/Terminal.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
+import { Terminal as XTerm } from "xterm";
+import "xterm/css/xterm.css";
+import Socket from "../functions/Socket";
 
 const Terminal: React.FC = () => {
-  return (
-    <div>
-      <p>Terminal</p>
-    </div>
-  );
+  const terminalRef = React.useCallback(node => {
+    if (node !== null) {
+      const terminal = new XTerm();
+      terminal.open(node);
+
+      const s = Socket.getInstance();
+      terminal.onData(data => {
+        s.getSocket().emit("pty-data", data);
+      });
+
+      s.getSocket().on("pty-data", (data: string) => {
+        terminal.write(data);
+      });
+    }
+  }, []);
+
+  return <div ref={terminalRef}></div>;
 };
 
 export default Terminal;
