b96cf5f1ad430cff50cb0b185a6e1fe174053de1
Michael <rehn.michael@outlook.com>
2019-11-19 13:59:00 +0100

    Add file watching (#61)
    * Add file watching
    
    * Fix lint erros

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,29 +1,48 @@
+import { promises as fs } from "fs";
+import { sep } from "path";
+import os from "os";
+
 import express from "express";
 import socketio from "socket.io";
 import fsTree from "./functions/fsTree";
+import Project from "./functions/Project";
 
-// This is the project root! In the future we will maybe move this to an
-// environment variable so we can customize it. Perhaps also
-// auto generate this folder if doesn't exist?
-const PROJECT_ROOT = ".";
+async function start(): Promise<void> {
+  // Initialize a project in a random temporary directory for now
+  const tmpDir = os.tmpdir();
+  const projectRoot = await fs.mkdtemp(`${tmpDir}${sep}`);
+  const project = new Project(projectRoot);
+  await project.initialize();
 
-const app = express();
-const port = 4000;
+  const app = express();
+  const port = 4000;
 
-app.get("/", (req, res) => {
-  res.send("YAY! It actually works!");
-});
+  app.get("/", (req, res) => {
+    res.send("YAY! It actually works!");
+  });
 
-const server = app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`Server is listening on ${port}`);
-});
+  const server = app.listen(port, err => {
+    if (err) {
+      return console.error(err);
+    }
+    return console.log(`Server is listening on ${port}`);
+  });
 
-const io = socketio(server);
-io.on("connection", async socket => {
-  console.log(`Socket with id ${socket.id} connected`);
+  const sockets: socketio.Socket[] = [];
+  const io = socketio(server);
+  io.on("connection", async socket => {
+    console.log(`Socket with id ${socket.id} connected`);
+    sockets.push(socket);
 
-  socket.emit("file-tree", await fsTree(PROJECT_ROOT));
-});
+    socket.emit("file-tree", await fsTree(projectRoot));
+  });
+
+  project.getWatcher().on("all", async () => {
+    const tree = await fsTree(projectRoot);
+    for (const socket of sockets) {
+      socket.emit("file-tree", tree);
+    }
+  });
+}
+
+start();
