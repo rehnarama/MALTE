5816d66e6a62c0aa01a77a1ecbbf7f9dbb952fe2
AdamInersjo <adam.inersjo@hotmail.com>
2019-11-20 09:44:28 +0100

    Create RGA.fromString() method

--- packages/rga/src/RGA.ts
+++ packages/rga/src/RGA.ts
@@ -166,4 +166,17 @@ export default class RGA {
     }
     return str;
   }
+
+  /**
+   * Converts a string into an RGA
+   * @param s string to convert
+   */
+  public static fromString(s: string): RGA {
+    const rga: RGA = new RGA();
+    for (let i = s.length - 1; i >= 0; i--) {
+      const insert = rga.createInsertPos(0, s[i]);
+      rga.insert(insert);
+    }
+    return rga;
+  }
 }
