fb6061fd652bdae01f405f32077c0d51a5ee1e79
AdamInersjo <Adam.Inersjo.2778@student.uu.se>
2019-11-20 09:54:22 +0100

    Fix Monaco error messages with temporary fix (#67)
    * Fix Monaco error messages with temporary fix
    
    * Remove commented out code
    
    Co-Authored-By: Michael <rehn.michael@outlook.com>

--- packages/frontend/src/components/CodeEditor.tsx
+++ packages/frontend/src/components/CodeEditor.tsx
@@ -1,28 +1,27 @@
 import React, { useRef } from "react";
-import { default as MonacoEditor } from "@monaco-editor/react";
-import * as monaco from "monaco-editor";
+import { ControlledEditor as MonacoEditor } from "@monaco-editor/react";
+import { editor as editorType } from "monaco-editor";
 import mapOperations, {
   printInternalOperations
 } from "../functions/MapOperations";
 
 const CodeEditor: React.FC = () => {
   const editorRef: React.MutableRefObject<
-    monaco.editor.ICodeEditor | undefined
+    editorType.ICodeEditor | undefined
   > = useRef();
 
   const handleEditorDidMount = (
     valueGetter: Function,
-    editor: monaco.editor.ICodeEditor
+    editor: editorType.ICodeEditor
   ): void => {
     console.log("Editor has loaded!");
     editorRef.current = editor;
 
-    const currentModel: monaco.editor.IEditorModel | null = editor.getModel();
+    const currentModel: editorType.IEditorModel | null = editor.getModel();
     if (currentModel) {
       // Set the EOL of the editor model to LineFeed
-      currentModel.pushEOL(monaco.editor.EndOfLineSequence.LF);
       currentModel.onDidChangeContent(
-        (event: monaco.editor.IModelContentChangedEvent) => {
+        (event: editorType.IModelContentChangedEvent) => {
           const op = event.changes[0];
           printInternalOperations(mapOperations(op));
         }
@@ -32,12 +31,20 @@ const CodeEditor: React.FC = () => {
     }
   };
 
+  const handleEditorChange = (
+    _: editorType.IModelContentChangedEvent,
+    value: string | undefined
+  ): string | undefined => {
+    return value ? value.replace("\r\n", "\n") : undefined;
+  };
+
   return (
     <MonacoEditor
       height="75vh"
       width="80vw"
       language="javascript"
       editorDidMount={handleEditorDidMount}
+      onChange={handleEditorChange}
     />
   );
 };
