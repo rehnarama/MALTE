eaee485b5aa64e99ede3da460b3d6e6d24915725
jowe3751 <jowe3751@student.uu.se>
2019-11-21 11:46:02 +0100

    Filetree now fetched from server, updates to filesystem reflected aswell (#80)
    * Use folder structure received from server
    
    * Remove socket stuff from index, no need to setup as we have Singleton
    
    * remove specific listener

--- packages/frontend/src/components/SideBar.tsx
+++ packages/frontend/src/components/SideBar.tsx
@@ -1,49 +1,35 @@
 import * as React from "react";
 import TreeNode from "malte-common/dist/TreeNode";
 import Tree from "./Tree";
-
-// example JSON
-const data: TreeNode = {
-  name: "root",
-  type: "directory",
-  path: "/tmp/root",
-  children: [
-    {
-      name: "folder1",
-      type: "directory",
-      path: "root",
-      children: [
-        {
-          name: "file1.js",
-          type: "file",
-          path: "root/folder1/file1.js"
-        },
-        { name: "file2.js", type: "file", path: "root/folder1/file2.js" }
-      ]
-    },
-    {
-      name: "file1.js",
-      type: "file",
-      path: "root/file1.js"
-    },
-    { name: "file2.js", type: "file", path: "root/file2.js" }
-  ]
-};
+import Socket from "../functions/Socket";
 
 interface State {
-  data: TreeNode;
+  data?: TreeNode;
   toggledKeys: { [key: string]: boolean };
 }
 
 class SideBar extends React.Component<{}, State> {
   constructor(props: {}) {
     super(props);
-    this.state = { data, toggledKeys: {} };
+    const socket = Socket.getInstance().getSocket();
+    this.state = { toggledKeys: {} };
+    socket.on("file-tree", this.onFileTree);
+  }
+
+  onFileTree = (data: TreeNode) => {
+    this.setState(() => ({ data }));
+  };
+
+  componentWillUnmount() {
+    Socket.getInstance()
+      .getSocket()
+      .removeListener("file-tree", this.onFileTree);
   }
 
   onSelect = (node: TreeNode) => {
     console.log("select", node);
   };
+
   onToggle = (node: TreeNode) => {
     const isToggled = this.state.toggledKeys[node.path] === true;
     const newToggledKeys = Object.assign({}, this.state.toggledKeys, {
@@ -56,13 +42,17 @@ class SideBar extends React.Component<{}, State> {
     return (
       <div>
         <p>Files</p>
-        <Tree
-          node={this.state.data}
-          root
-          toggledKeys={this.state.toggledKeys}
-          onSelect={this.onSelect}
-          onToggle={this.onToggle}
-        />
+        {this.state.data ? (
+          <Tree
+            node={this.state.data}
+            root
+            toggledKeys={this.state.toggledKeys}
+            onSelect={this.onSelect}
+            onToggle={this.onToggle}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     );
   }
