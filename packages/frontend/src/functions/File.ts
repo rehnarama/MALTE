33e665a842bb7c57c448f93c4a2049de0f61be91
Michael <rehn.michael@outlook.com>
2019-11-27 15:59:50 +0100

    Stream operations to all clients (#125)
    * Remove console log
    
    * Add application of remote editing
    
    * Remove console log
    
    * Remove unused imports
    
    * Fix lint errors

--- packages/frontend/src/functions/File.ts
+++ packages/frontend/src/functions/File.ts
@@ -1,8 +1,17 @@
-import RGA, { RGAJSON } from "rga/dist/RGA";
+import RGA, {
+  RGAJSON,
+  RGAOperationJSON,
+  rgaOperationFromJSON
+} from "rga/dist/RGA";
 import { editor as editorType, IDisposable } from "monaco-editor";
 import mapOperations from "./MapOperations";
 import { InternalOperation, Operation } from "malte-common/dist/Operations";
 import Socket from "./Socket";
+
+interface BufferOperationData {
+  path: string;
+  operation: RGAOperationJSON;
+}
 
 export default class File {
   private _path: string;
@@ -16,6 +25,7 @@ export default class File {
   }
 
   private contentChangedListener: IDisposable;
+  private applyingRemote = false;
 
   constructor(path: string, content: RGAJSON, model: editorType.ITextModel) {
     this._path = path;
@@ -25,11 +35,31 @@ export default class File {
 
     this.contentChangedListener = this.model.onDidChangeContent(
       (event: editorType.IModelContentChangedEvent) => {
+        if (this.applyingRemote) {
+          return;
+        }
         const op = event.changes[0];
         const internalOps = mapOperations(op);
         this.applyLocalOperations(internalOps);
       }
     );
+
+    Socket.getInstance()
+      .getSocket()
+      .on("buffer-operation", (data: BufferOperationData) => {
+        if (data.path === this.path) {
+          const operation = rgaOperationFromJSON(data.operation);
+          if ("content" in operation) {
+            this.rga.insert(operation);
+          } else {
+            this.rga.remove(operation);
+          }
+
+          this.applyingRemote = true;
+          model.setValue(this.toString());
+          this.applyingRemote = false;
+        }
+      });
   }
 
   private internalToRGA(op: InternalOperation) {
@@ -44,7 +74,6 @@ export default class File {
     for (const op of ops) {
       const rgaOp = this.internalToRGA(op);
       this.rga.applyOperation(rgaOp);
-      console.log(this.rga.toString());
       const socket = Socket.getInstance().getSocket();
       socket.emit("buffer-operation", { path: this.path, operation: rgaOp });
     }
