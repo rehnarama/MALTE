2af94a38a532be180bf50405b95d46d88267d314
AdamInersjo <Adam.Inersjo.2778@student.uu.se>
2019-12-02 10:55:18 +0100

    Refactor frontend editor code for easier testing (#129)
    * Refactor CodeEditor code into Editor class
    
    * Fix CodeEditor not calling initialize on Editor

---
+++ packages/frontend/src/functions/Editor.ts
@@ -0,0 +1,38 @@
+import { editor as editorType } from "monaco-editor";
+import File from "./File";
+import Monaco from "./Monaco";
+import { RGAJSON } from "rga/dist/RGA";
+import Socket from "../functions/Socket";
+
+export default class Editor {
+  private editor: editorType.ICodeEditor;
+  private files: File | undefined;
+  private editorNamespace: typeof editorType;
+
+  constructor(editor: editorType.ICodeEditor) {
+    this.editor = editor;
+    this.editorNamespace = Monaco.getInstance().getEditorNamespace();
+  }
+
+  public initialize() {
+    const socket = Socket.getInstance().getSocket();
+    socket.on("open-buffer", (data: { path: string; content: RGAJSON }) => {
+      if (this.files !== undefined) {
+        // Close previously open file
+        this.files.close();
+      }
+      this.openNewBuffer(data.path, data.content);
+    });
+    socket.emit("join-buffer", { path: "tmp.js" });
+  }
+
+  private openNewBuffer(path: string, content: RGAJSON) {
+    const newModel = this.editorNamespace.createModel("", "javascript");
+    newModel.pushEOL(this.editorNamespace.EndOfLineSequence.LF);
+
+    const file = new File(path, content, newModel);
+    this.files = file;
+
+    this.editor.setModel(newModel);
+  }
+}
--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import Monaco from "./functions/Monaco";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Monaco needs to be initialized before anythin can run
+const m = Monaco.getInstance();
+m.initialize().then(() => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
