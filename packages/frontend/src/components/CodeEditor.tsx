e50ae144cf32713596ba13d9ecfe10d4137cdd83
Michael Rehn <rehn.michael@outlook.com>
2019-12-04 11:14:26 +0100

    Fix resizing wonkyness

--- packages/frontend/src/components/CodeEditor.tsx
+++ packages/frontend/src/components/CodeEditor.tsx
@@ -3,8 +3,12 @@ import MonacoEditor from "@monaco-editor/react";
 import { editor as editorType } from "monaco-editor";
 import Editor from "../functions/Editor";
 import classes from "./CodeEditor.module.css";
+import ReactResizeDetector from "react-resize-detector";
 
 const CodeEditor: React.FC = () => {
+  const [width, setWidth] = React.useState<number | undefined>();
+  const [height, setHeight] = React.useState<number | undefined>();
+
   const editorRef = useRef<Editor | undefined>();
   const [fileName, setFileName] = React.useState("");
 
@@ -14,11 +18,37 @@ const CodeEditor: React.FC = () => {
     editorRef.current = e;
   };
 
+  function resizeTerminal(width: number, height: number) {
+    /* console.log(width, height); */
+    setWidth(width);
+    setHeight(height);
+  }
+
   return (
-    <>
-      <div className={classes.filename}>{fileName}</div>
-      <MonacoEditor language="javascript" editorDidMount={handler} />
-    </>
+    <div
+      style={{
+        height: "100%",
+        display: "grid",
+        gridTemplateRows: "min-content 1fr"
+      }}
+    >
+      <div className={classes.tabBar}>
+        <p className={classes.fileName}>{fileName}</p>
+      </div>
+      <div style={{ minWidth: 0, minHeight: 0 }}>
+        <ReactResizeDetector
+          handleWidth
+          handleHeight
+          onResize={resizeTerminal}
+        />
+        <MonacoEditor
+          width={width}
+          height={height}
+          language="javascript"
+          editorDidMount={handler}
+        />
+      </div>
+    </div>
   );
 };
 
