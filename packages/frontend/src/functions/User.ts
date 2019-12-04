0bbe46372eda1c1e5e6ea3c42d8326c4e67687df
jowe3751 <jowe3751@student.uu.se>
2019-12-04 14:25:13 +0100

    Update packages/frontend/src/functions/User.ts
    Co-Authored-By: Jonas Norlinder <noler@users.noreply.github.com>

--- packages/frontend/src/functions/User.ts
+++ packages/frontend/src/functions/User.ts
@@ -74,7 +74,6 @@ export default class User {
     const regex = /(userId)=([^;]+)/g;
     const userId = regex.exec(document.cookie);
     if (userId && userId[2]) {
-      console.log(userId[2]);
       Socket.getInstance()
         .getSocket()
         .emit("join-group", userId[2]);
