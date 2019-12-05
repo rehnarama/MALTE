f88b889127dcf6e04f478ff339e1faf030c35e6f
AdamInersjo <adam.inersjo@hotmail.com>
2019-12-05 10:33:50 +0100

    Create editor welcome screen

--- packages/frontend/src/components/CodeEditor/WelcomeScreen.tsx
+++ packages/frontend/src/components/CodeEditor/WelcomeScreen.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
+import classes from "./WelcomeScreen.module.css";
 
 const WelcomeScreen: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to MALTE!</h1>
+    <div className={classes.container}>
+      <div className={classes.title}>
+        <p>Welcome to MALTE!</p>
+      </div>
+      <div>
+        <p>Please select a file to the left to get started!</p>
+      </div>
     </div>
   );
 };
