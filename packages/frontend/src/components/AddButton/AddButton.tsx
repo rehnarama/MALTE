5997fc2f9fbbbf59d72bd87bf3e8a88b0b571310
Joel <jowe3751@student.uu.se>
2019-12-06 13:44:22 +0100

    Add support for adding and removing pre-approved users on frontend

---
+++ packages/frontend/src/components/AddButton/AddButton.tsx
@@ -0,0 +1,98 @@
+import * as React from "react";
+import addsvg from "./add.svg";
+import deletesvg from "./delete.svg";
+import Avatar from "../Avatar";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import classes from "./AddButton.module.css";
+import { Button } from "@material-ui/core";
+
+const AddButton: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+  const [users, setUsers] = React.useState<string[]>([]);
+  const [userName, setUserName] = React.useState("");
+
+  const handleFileNameChange = React.useCallback<
+    React.ChangeEventHandler<HTMLInputElement>
+  >(e => {
+    setUserName(e.currentTarget.value);
+  }, []);
+
+  const handleKeyDown = React.useCallback<React.KeyboardEventHandler>(
+    e => {
+      if (e.key === "Enter") {
+        addUser();
+        console.log(userName);
+      } else if (e.key === "Escape") {
+        e.stopPropagation();
+        setUserName("");
+      }
+    },
+    [userName]
+  );
+
+  const onSelect = () => {
+    setOpen(true);
+  };
+
+  const addUser = () => {
+    // This function will call backend
+    if (userName && !users.includes(userName)) {
+      setUsers(users.concat(userName));
+      setUserName("");
+    }
+  };
+
+  const removeUser = (userName: string) => {
+    // This function will call backend
+    if (userName) {
+      setUsers(users.filter(e => e !== userName)); 
+      setUserName("");
+    }
+  };
+
+  return (
+    <>
+      <Dialog
+        onClose={() => setOpen(false)}
+        aria-labelledby="simple-dialog-title"
+        open={open}
+      >
+        <DialogTitle id="simple-dialog-title">Collaborators</DialogTitle>
+        <div className={classes.collaboratorList}>
+          {users.length > 0 ? (
+            <ul>
+              {users.map(collaborator => (
+                <li>
+                  {collaborator}
+                  <img
+                    key="delete"
+                    src={deletesvg}
+                    onClick={() => removeUser(collaborator)}
+                    alt="Delete"
+                  />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No users</p>
+          )}
+        </div>
+        <input
+          type="text"
+          autoFocus
+          placeholder="Enter username"
+          onKeyDown={handleKeyDown}
+          onChange={handleFileNameChange}
+          value={userName}
+        />
+        <Button onClick={addUser}>Add User</Button>
+        <Button fullWidth variant={"outlined"} onClick={() => setOpen(false)}>
+          Close
+        </Button>
+      </Dialog>
+      <Avatar url={addsvg} name={"Add User"} onSelect={onSelect} />
+    </>
+  );
+};
+export default AddButton;
