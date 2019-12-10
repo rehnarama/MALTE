b425e8f80da1bd6915c1264956a70b27fe23e25f
Joel <jowe3751@student.uu.se>
2019-12-10 11:16:54 +0100

    PreApprovedUser

---
+++ packages/backend/src/functions/oauth/PreApprovedUser.ts
@@ -0,0 +1,27 @@
+import Database from "../db/Database";
+
+export async function addPreApproved(login: string): Promise<void> {
+  console.log("add: ", login);
+  const collection = Database.getInstance()
+    .getDb()
+    .collection("preapproved");
+  if (!(await collection.findOne({ login: login }))) {
+    collection.insertOne({ login: login });
+  }
+}
+
+export async function removePreApproved(login: string): Promise<void> {
+  console.log("remove: ", login);
+  const collection = Database.getInstance()
+    .getDb()
+    .collection("preapproved");
+  collection.deleteOne({ login: login });
+}
+
+export async function getAllPreapproved(): Promise<string[]> {
+  const collection = Database.getInstance()
+    .getDb()
+    .collection("preapproved");
+  const preApprovedUsers = await collection.find().toArray();
+  return preApprovedUsers.map(user => user.login);
+}
