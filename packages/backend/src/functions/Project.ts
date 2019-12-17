eed66593f0cb0fc071811e6e8464e8445e7b4375
Jonas Norlinder <jonas@norlinder.nu>
2019-12-17 10:55:35 +0100

    Fix faulty removal and code clean up (#214)
    * Fix faulty removal and code clean up
    
    * Update Project.ts
    
    * Update packages/backend/src/functions/Project.ts
    
    Co-Authored-By: Michael <rehn.michael@outlook.com>
    
    * Update Project.ts
    
    * Lint fic

--- packages/backend/src/functions/Project.ts
+++ packages/backend/src/functions/Project.ts
@@ -168,23 +168,25 @@ export default class Project {
     }
   }
 
-  public async removeSocket(socket: SocketIO.Socket): Promise<void> {
+  public removeSocket(socket: SocketIO.Socket): boolean {
     const index = this.sockets.findIndex(s => s.id === socket.id);
-    if (index) {
+    if (index !== -1) {
       this.sockets.splice(index, 1);
+      if (this.cursorMap[socket.id]) {
+        delete this.cursorMap[socket.id];
+        this.broadcastCursorList();
+      }
       this.broadcastUserList();
-    }
-    if (this.cursorMap[socket.id]) {
-      delete this.cursorMap[socket.id];
-      this.broadcastCursorList();
-    }
 
-    const filesToClose: File[] = [];
-    for (const file of this.files) {
-      const canCloseFile = file.leave(socket);
-      if (canCloseFile) filesToClose.push(file);
+      const filesToClose: File[] = [];
+      for (const file of this.files) {
+        const canCloseFile = file.leave(socket);
+        if (canCloseFile) filesToClose.push(file);
+      }
+      this.closeFiles(filesToClose);
+      return true;
     }
-    await this.closeFiles(filesToClose);
+    return false;
   }
 
   private broadcastCursorList(): void {
