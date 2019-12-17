1832c2defeae9e36ed727b4bc228efee58a87a6b
Jonas Norlinder <jonas@norlinder.nu>
2019-12-17 10:51:34 +0100

    Lint fic

--- packages/malte-common/src/UserList.ts
+++ packages/malte-common/src/UserList.ts
@@ -1,6 +1,6 @@
 import { User as UserData } from "./oauth/GitHub";
 
-type User = UserData & {socketId: string };
+type User = UserData & { socketId: string };
 export { User };
 
 export interface UserList {
